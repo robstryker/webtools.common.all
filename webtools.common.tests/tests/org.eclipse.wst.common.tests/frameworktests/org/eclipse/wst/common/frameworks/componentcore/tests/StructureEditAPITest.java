64ad3c89897076ed572dbc9703982ba649222d91
cbridgha <cbridgha>
2006-01-18 18:46:31 +0000

    [108837] Structure edit sychronization

--- webtools.common.tests/tests/org.eclipse.wst.common.tests/frameworktests/org/eclipse/wst/common/frameworks/componentcore/tests/StructureEditAPITest.java
+++ webtools.common.tests/tests/org.eclipse.wst.common.tests/frameworktests/org/eclipse/wst/common/frameworks/componentcore/tests/StructureEditAPITest.java
@@ -2,16 +2,21 @@ package org.eclipse.wst.common.frameworks.componentcore.tests;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.internal.jobs.JobManager;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.wst.common.componentcore.ComponentCore;
 import org.eclipse.wst.common.componentcore.UnresolveableURIException;
+import org.eclipse.wst.common.componentcore.internal.ComponentcoreFactory;
+import org.eclipse.wst.common.componentcore.internal.Property;
 import org.eclipse.wst.common.componentcore.internal.StructureEdit;
 import org.eclipse.wst.common.componentcore.internal.WorkbenchComponent;
 import org.eclipse.wst.common.frameworks.componentcore.virtualpath.tests.TestWorkspace;
@@ -22,8 +27,10 @@ public class StructureEditAPITest extends TestCase {
 	public static final String WEB_MODULE_NAME = "WebModule1";
 	public static final URI moduleURI = URI.createURI("module:/resource/TestArtifactEdit/WebModule1");
 	public static final String EDIT_MODEL_ID = "jst.web";
+	public static final String EDITMODEL_STRESS = "stresstest";
 	private Path zipFilePath = new Path("TestData" + fileSep + "TestArtifactEdit.zip");
 	private IProject project;
+	
 
 
 	// /This should be extracted out, dont have time, just trying to get coverage
@@ -279,32 +286,53 @@ public class StructureEditAPITest extends TestCase {
 
 	public void testMultiThreadAccess() {
 		
-		Thread[] testJobs = new Thread[200];
+		Job[] testJobs = new Job[50];
 		for (int i = 0; i < testJobs.length; i++) {
-			Thread job = new Thread("Job " + i)
+			Job job = new Job("Job " + i)
 		      {
 		        
 		        protected IStatus run(IProgressMonitor monitor)
 		        {
+		        StructureEdit moduleCore = null;
 		          try
 		          {
-		        	StructureEdit moduleCore = StructureEdit.getStructureEditForRead(project);
-		      		System.out.println(moduleCore.getWorkbenchModules());
-		      		moduleCore.dispose();
+		        	moduleCore = StructureEdit.getStructureEditForWrite(project);
+		        	Property prop = ComponentcoreFactory.eINSTANCE.createProperty();
+		        	prop.setName("Job " + System.currentTimeMillis());
+		        	prop.setValue("Blah");
+		      		moduleCore.getComponent().getProperties().add(prop);
+		      		System.out.println(prop.getName());
+		      		moduleCore.saveIfNecessary(null);
 		          }
 		          catch (Exception e)
 		          {
 		        	  e.printStackTrace();
 		        	  return Status.CANCEL_STATUS;
 		          }
+		          finally {
+		        	  if (moduleCore != null)
+		        		  moduleCore.dispose();
+		          }
 		          return Status.OK_STATUS;
 		        }
+		        public boolean belongsTo(Object family) {
+					return EDITMODEL_STRESS.equals(family);
+				}
 		      };
 			testJobs[i] = job;	
 			}
 		for (int j = 0; j < testJobs.length; j++) {
-			Thread job = testJobs[j];
-			job.run();
+			Job job = testJobs[j];
+			job.schedule();
+		}
+		try {
+			JobManager.getInstance().join(EDITMODEL_STRESS,null);
+		} catch (OperationCanceledException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 	}
